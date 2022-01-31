efe59fb460a5002db71a875c46335d0aa079ddff
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-01-30 23:44:40 -0800

    needed to rollback and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2022_01_31_074134) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.integer "age"
+    t.boolean "competitive"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
