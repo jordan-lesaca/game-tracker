6580f4478bf6d7f770157f966f165e8d61e9f98a
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-01-31 02:41:48 -0800

    added update method and params for Games Controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,26 @@
 class GamesController < ApplicationController
+    #fullCRUD
 
-    def index
+    def index #Render All
         games = Game.all
         render json: games
     end
 
-    def create
+    def create #Create One
         game = Game.create
         render json: game
     end
 
+    def update
+        game = Game.find(params[:id])
+        game.update
+        render json: game
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:title, :release_year, :genre, :user_id)
+    end
 
 end
