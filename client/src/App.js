fa8d4368f6624e8680c8b4555317840bc22411d8
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-01-31 21:20:43 -0800

    added login form to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import { useState, useEffect } from 'react'
+import LoginForm from './LoginForm'
 import Games from './Games'
 
 function App() {
   const [user, setUser] = useState(null)
 
+  useEffect(() => {
+    fetch("/me")
+    .then((r) => {
+      if(r.ok){
+        r.json().then((user) => setUser(user))
+      }
+    })
+  },[])
+
+  function handleLogoutClick(){
+    fetch("/logout", { 
+      method: "DELETE" 
+    })
+     .then(r => {setUser(null)
+    })
+  }
+
+  if (!user) return <LoginForm setUser={setUser} />
+
   return (
     <div className="App">
       <header className="App-header">
         <Games user={user} />
+        <button onClick={handleLogoutClick}>LOG OUT</button>
       </header>
     </div>
   );
