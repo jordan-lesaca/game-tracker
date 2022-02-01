c535e0a42010bea00ba6f05b6a478f4e76801c4b
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-01-31 19:06:21 -0800

    added routes for users, login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   
   resources :games
   resources :users
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
