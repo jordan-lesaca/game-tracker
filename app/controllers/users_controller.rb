17caa2c9f64503cd4e6372b1a0c6c780bf84a2a7
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-01-31 18:01:57 -0800

    added show method to user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,11 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    def show
+        user = User.(params[:id])
+        render json: user
+    end
+
+    
+
 end
