6bb4c2c5736b99a2f1695aaed9af9f9aa708c7ce
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-02-01 12:12:02 -0800

    added comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
-    
+    # skip_before_action :authorize, only: :create    
+
     def create #Login
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id
@@ -11,3 +12,18 @@ class SessionsController < ApplicationController
     end
 
 end
+
+    # #Login
+    # def create 
+    #     user = User.find_by(username: params[:username]) #When we find the user, 
+    #     #we are setting that ID equal to that user.id key to that session hash
+    #     session[:user_id] = user.id#user_id. This makes sure user exist in db. 
+    #     #Sets the sessions ID equal to the user id. 
+    #     render json: user
+    # end
+
+    # #Logout
+    # def destroy
+    #     session.delete :user_id 
+    #     head :no_content
+    # end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,3 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :age, :competitive
   has_many :games, serializer: GameSerializer
 end
+
+  #specifies to user serializer that it should be using this serializer instead (passing it explicitly)
+  #We use serializer: NameSerializer to specify what details of the database we want to render. 
